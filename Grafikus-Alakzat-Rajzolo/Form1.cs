d410eec1398e8c6a595ec52d3afc911112e097ca
AsuraMTG <eper.karma@gmail.com>
2025-02-25 08:36:04 +0100

    v1

--- Grafikus-Alakzat-Rajzolo/Form1.cs
+++ Grafikus-Alakzat-Rajzolo/Form1.cs
@@ -12,11 +12,10 @@ namespace Grafikus_Alakzat_Rajzolo
 {
     public partial class Form1 : Form
     {
-        private int currentSides = 3;    // Kezdetben háromszöget rajzolunk
-        private float opacity = 0.0f;     // Kezdő átlátszóság, hogy halványan jelenjen meg
+        private int currentSides = 3;
+        private float opacity = 0.0f;
         private Timer timer;
-        private int frameCounter = 0;    // Framelépések száma
-        private int maxSides = 12;       // Maximális oldalszám (például 12 oldalú sokszög)
+        private int frameCounter = 0;
         private bool fadingIn = true;
 
         public Form1()
@@ -24,78 +23,67 @@ namespace Grafikus_Alakzat_Rajzolo
             InitializeComponent();
 
             timer = new Timer();
-            timer.Interval = 100; // 100 ms (0.1 másodpercenként) történik az animáció
+            timer.Interval = 100;
             timer.Tick += Timer_Tick;
             timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            // A kép mérete
             int width = pictureBox2.Width;
             int height = pictureBox2.Height;
 
-            // Kép készítése a PictureBox-on
             Bitmap bmp = new Bitmap(width, height);
             Graphics g = Graphics.FromImage(bmp);
-            g.Clear(Color.White);  // Minden animációs lépésnél töröljük a képet
+            g.Clear(Color.White);
 
-            // A szín beállítása (halványodás az átlátszóság segítségével)
-            Color color = Color.FromArgb((int)(opacity * 255), Color.Black); // Piros szín
+            Color color = Color.FromArgb((int)(opacity * 255), Color.Black);
             Brush brush = new SolidBrush(color);
 
-            // Poligon csúcsainak kiszámítása
             PointF[] points = CalculatePolygonPoints(currentSides, width, height);
 
-            // Sokszög kirajzolása
-            g.FillPolygon(brush, points);  // A sokszög kitöltése
-            g.DrawPolygon(Pens.White, points);   // A sokszög körvonalának megrajzolása
+            g.FillPolygon(brush, points);
+            g.DrawPolygon(Pens.White, points);
 
-            // A képet a PictureBox-ra állítjuk
             pictureBox2.Image = bmp;
 
-            // Ha megjelenik (fadingIn == true), növeljük az átlátszóságot, ha eltűnik (fadingIn == false), csökkentjük
             if (fadingIn)
             {
-                opacity += 0.05f;  // Növeljük az átlátszóságot
-                if (opacity >= 1.0f)  // Ha teljesen látható, akkor váltunk az eltűnésre
+                opacity += 0.05f;
+                if (opacity >= 1.0f)
                 {
                     fadingIn = false;
                 }
             }
             else
             {
-                opacity -= 0.05f;  // Csökkentjük az átlátszóságot
-                if (opacity <= 0)  // Ha teljesen eltűnt, új sokszöget rajzolunk
+                opacity -= 0.05f;
+                if (opacity <= 0)
                 {
-                    if (frameCounter < 3) // Csak három sokszöget jelenítünk meg
+                    if (frameCounter < 3)
                     {
-                        frameCounter++;  // Következő sokszög
-                        opacity = 0.0f;  // Új sokszög indul átlátszóként
-                        currentSides++;  // Növeljük az oldalszámot
-                        fadingIn = true;  // A következő sokszög újra meg fog jelenni
+                        frameCounter++;
+                        opacity = 0.0f;
+                        currentSides++;
+                        fadingIn = true;
                     }
                     else
                     {
-                        timer.Stop(); // Animáció befejezése
+                        timer.Stop();
                     }
                 }
             }
         }
         private PointF[] CalculatePolygonPoints(int sides, int width, int height)
         {
-            // A sokszög középpontja
             PointF center = new PointF(width / 2, height / 2);
 
-            // A sugár fixálása, hogy a méret ne változzon
             float radius = Math.Min(width, height) / 3;
 
-            // Csúcsok kiszámítása
             PointF[] points = new PointF[sides];
             for (int i = 0; i < sides; i++)
             {
-                // Az egyes szögeket a középpont körül egyenletesen osztjuk el
-                float angle = (float)(i * 2 * Math.PI / sides); // Szög az egyes csúcsokhoz
+                float angle = (float)(i * 2 * Math.PI / sides);
                 points[i] = new PointF(
                     center.X + radius * (float)Math.Cos(angle),
                     center.Y + radius * (float)Math.Sin(angle)
